fc0636b3181fadc3a127a9c8bb7fae1b6e1cdb92
lokesh <lokeshxai@gmail.com>
2026-02-24 12:19:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,3 +33,15 @@ toggle.addEventListener("click", () => {
     localStorage.setItem("theme", "light");
   }
 });
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach((element) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      element.classList.add("active");
+    }
+  });
+});
